e118af29d69cf1700aa7542874b4ee241103611b
Benkolon152 <v.benasz@gmail.com>
2025-09-18 09:51:36 +0200

    implement insertion

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,13 +9,29 @@ export default class App extends React.Component {
       ]
     },
     nev: null,
-    ar: null,
+    ar: 0,
     meny: null,
   }
 
   handleButtonClick = e => {
     e.preventDefault();
+
+    console.log('Button clicked!', this.state);
+    if (this.state.nev && this.state.ar && this.state.ar > 0 && this.state.meny && this.state.meny > 0) {
+      this.setState({
+        cart: {
+          items: [...this.state.cart.items,
+            {
+              name: this.state.nev,
+              price: +this.state.ar,
+              quantity: +this.state.meny
+            }
+          ]
+        }})
+  } else{
+      alert('Hibás adatok!');
   }
+}
 
   render() {
     return(
@@ -23,16 +39,16 @@ export default class App extends React.Component {
           <form>
             <h1>Termék hozzáadása a kosárhoz</h1>
             <label htmlFor="input_nev">Termék neve</label>
-            <input type="text" id="input_nev" name="input_nev" placeholder="Név..."
-            onChange={value => this.setState({nev: value})}
+            <input type="text" id="input_nev" name="input_nev" placeholder="Név..." required
+            onChange={e => this.setState({nev: e.target.value})}
             /><br />
             <label htmlFor="input_ar">Termék ára</label>
-            <input type="number" id="input_ar" name="input_ar" value={0}
-            onChange={value => this.setState({ar: value})}
+            <input type="number" id="input_ar" name="input_ar" value={this.state.ar}
+            onChange={e => this.setState({ar: +e.target.value})}
             /><br />
             <label htmlFor="input_meny">Termék mennyisége</label>
-            <input type="text" id="input_meny" name="input_meny" placeholder="Mennyiség..."
-            onChange={value => this.setState({meny: value})}
+            <input type="text" id="input_meny" name="input_meny" placeholder="Mennyiség..." required
+            onChange={e => this.setState({meny: +e.target.value})}
             /><br />
             <input type="button" id="btn_submit" name="btn_submit" value="Hozzáadás a kosárhoz" onClick={this.handleButtonClick}/>
           </form>
